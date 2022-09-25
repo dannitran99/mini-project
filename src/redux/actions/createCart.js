f6bf7bd6a524196b4f7c607d20583835032369d2
minh <dannitran99@gmail.com>
2022-09-25 15:44:09 +0700

    cart

--- src/redux/actions/createCart.js
+++ src/redux/actions/createCart.js
@@ -21,7 +21,7 @@ export const createCart = createSlice({
         if(notFound) state.data.push({id:action.payload.id, title:action.payload.title, price:action.payload.price,number:1});
     },
     plusProduct: (state, action) => {
-      state.data.find(element => {
+      state.data.map(element => {
           if (element.id === action.payload) {
               element.number++;
           }
@@ -29,10 +29,9 @@ export const createCart = createSlice({
       state.count++;
     },
     minusProduct: (state, action) => {
-      state.data.find((element,index) => {
-          
+      state.data.map((element,index) => {
           if (element.id === action.payload) {
-            console.log(element.id)
+            console.log(element.id,action.payload)
             element.number ==1?state.data.splice(index, 1):element.number--;
           }
         });
